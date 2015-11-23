d2dc4a094a183070bdc57a9279d299156c828ab5
Mike Yurchenkov <mayurchenkov@edu.hse.ru>
2015-11-23 17:59:43 +0300

    added seeds

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Car.create(name: "Toyota-huyota", lat: 55.0, lng: 37.0, price: 5000, class: 1, seats:4, luggage: 3, conditioning: false, transmission: 3, availability_start_date: Date.today, availability_end_date: Date.today + 2.days)
