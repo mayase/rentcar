0b2ae8ffe6d077c36c33a2c6666729e3466f58d9
Mike Yurchenkov <mikesehse@gmail.com>
2015-11-23 21:40:56 +0300

    routes fix

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :cars, except: [:new, :edit], path: 'api/cars'
+  get 'api/cars/search', to: 'cars#search'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
