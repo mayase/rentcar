18efeaa01cf25f0a2a7696289545da14b8e4ebda
Maxim <mail4mayase@gmail.com>
2015-11-23 17:16:08 +0300

    client apiTest

--- client/source/app/api_test/ctrl.js
+++ client/source/app/api_test/ctrl.js
@@ -2,9 +2,30 @@
     var module_name = 'Rentcar.ApiTest',
         controller_name = 'IndexController';
     angular.module(module_name).controller(module_name+'.'+controller_name, controller);
-    controller.$inject = ['$scope', '$state'];
-    function controller($scope, $state) {
+    controller.$inject = ['$scope', '$state', 'Restangular'];
+    function controller($scope, $state, Restangular) {
 
         var self = this;
+
+        self.searchFormData = {
+
+        };
+        var searchResultValues = {
+            start: 'Введите параметры и нажмите кнопку "Go"',
+            error: 'Произошла ошибка. Подробности в консоли'
+        };
+        self.searchResult=searchResultValues.start;
+
+        self.searchRequest = function(){
+            console.log(self.searchFormData);
+            Restangular.all('api').one('cars/search').get(self.searchFormData).then(function(data){
+                self.searchResult = data.plain();
+            },
+                function(error){
+                    console.log(error);
+                    self.searchResult = searchResultValues.error;
+                }
+            )
+        }
     }
 }());
